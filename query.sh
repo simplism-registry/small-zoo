2e0635037257ea624f75c460c4128fe2adc3f600
Philippe Charrière <ph.charriere@gmail.com>
2023-12-23 20:18:00 +0000

    📦 small_zoo v0.0.0

--- query.sh
+++ query.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-curl http://localhost:8080 \
+curl http://localhost:8080/ant \
 -H 'content-type: application/json; charset=utf-8' \
--d '{"firstName":"Bob","lastName":"Morane"}'
+-d 'Bob Morane'
+
+curl http://localhost:8080/cow \
+-H 'content-type: application/json; charset=utf-8' \
+-d 'Bill Balantine'
+
+curl http://localhost:8080/elephant \
+-H 'content-type: application/json; charset=utf-8' \
+-d 'John Doe'
+
+curl http://localhost:8080/tiger \
+-H 'content-type: application/json; charset=utf-8' \
+-d 'Jane Doe'
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,50 @@
 use extism_pdk::*;
 use serde::{Serialize, Deserialize};
 
+pub fn get_ant(input: String) -> String {
+format!(r#"
+/\/\
+    \_\  _..._
+    (" )(_..._)
+    ^^  // \\
+{}
+"#, input).to_string()
+}
+
+pub fn get_cow(input: String) -> String {
+format!(r#"
+^__^
+(oo)\_______
+(__)\       )\/\
+    ||----w |
+    ||     ||
+{}
+"#, input).to_string()
+}
+
+pub fn get_elephant(input: String) -> String {
+format!(r#"
+  _    _
+ /=\""/=\
+(=(0_0 |=)__
+ \_\ _/_/   )
+   /_/   _  /\
+  |/ |\ || |
+     ~ ~  ~
+{}
+"#, input).to_string()
+}
+    
+pub fn get_default(input: String) -> String {
+format!(r#"
+{}
+"#, input).to_string()
+}
 
 // return a hashmap of array of strings
 fn headers_map() ->     std::collections::HashMap<String, Vec<String>> {
     let mut headers = std::collections::HashMap::new();
-    headers.insert("Content-Type".to_string(), vec!["application/json".to_string()]);
+    headers.insert("Content-Type".to_string(), vec!["text/plain".to_string()]);
     headers
 }
 
@@ -31,7 +70,13 @@ pub fn handle(input: String) -> FnResult<Json<ResponseData>> {
 
     let req: RequestData = serde_json::from_str(&input).unwrap();
 
-    let message: String = "👋 Hello ".to_string() + &req.body;
+    // see https://www.asciiart.eu/
+    let message: String = match req.uri.as_str() {
+        "/ant" => get_ant(req.body),
+        "/cow" => get_cow(req.body),
+        "/elephant" => get_elephant(req.body),
+        _ => get_default(req.body),
+    };
 
     let resp = ResponseData { 
         body: message , 
